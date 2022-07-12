06df251a22a2dcca7fc5a0eaaee8945753807f17
Laura Petrola <46495368+petrolau@users.noreply.github.com>
2022-07-12 17:44:32 -0300

    add MainContent

---
+++ my-app/src/components/MainContent/index.js
@@ -0,0 +1,15 @@
+import { StyledDiv } from "./styles";
+
+const MainContent = () => {
+  return (
+    <StyledDiv>
+      <h1>Online Experience</h1>
+      <span>
+        Join unique interactive activities led by one-of-a-king hosts--all
+        without leaving home.
+      </span>
+    </StyledDiv>
+  );
+};
+
+export default MainContent;
