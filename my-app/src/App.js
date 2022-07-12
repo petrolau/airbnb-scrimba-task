1bc30141ab41d7484c375f2a7e30d8eaeb9f7bb5
Laura Petrola <46495368+petrolau@users.noreply.github.com>
2022-07-12 17:43:58 -0300

    init project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import { StyledBody } from "./components/StyledBody/styles";
+import Hero from "./components/Hero/index";
+import MainContent from "./components/MainContent";
+import ImageGrid from "./components/ImageGrid";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledBody>
+      <Navbar />
+      <Hero />
+      <MainContent />
+      <ImageGrid />
+    </StyledBody>
   );
 }
 
